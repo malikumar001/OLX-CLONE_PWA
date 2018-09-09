e670a32eb12697dbf01d86c2ec30588ac788abee
malikumar001 <umawtips1@gmail.com>
2018-09-09 09:15:29 +0500

    service worker problem

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey:process.env.COOKIE_KEY 
+    mongoURI: 'mongodb://olx-clone:getabc12345@ds249942.mlab.com:49942/olx-clone',
+    cookieKey: 'sdfsfadf34252345dswed345' 
 }
 
 
-// mongodb://olx-clone:getabc12345@ds249942.mlab.com:49942/olx-clone
+// 
