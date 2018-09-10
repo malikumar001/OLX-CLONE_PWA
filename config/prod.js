72642eec24553ffd2726b3a1240cab1fed54ff2d
malikumar001 <umawtips1@gmail.com>
2018-09-10 15:30:15 +0500

    df

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,10 @@
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    googleClientID: '569225356441-873f9t6e2pjv643p5d3b6h7ff3co33fn.apps.googleusercontent.com',
+    googleClientSecret: '7tzWtjBlF1U9sySOUB1jIIFR',
     mongoURI: 'mongodb://olx-clone:getabc12345@ds249942.mlab.com:49942/olx-clone',
-    cookieKey: 'sdfsfadf34252345dswed345' 
+    cookieKey: 'dsfgjhdfjjdkgyg89745jkgh'
 }
+
 
 
 
