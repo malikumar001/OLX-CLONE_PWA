abd52091ada5699c47a7a77500af7da50bcedb13
malikumar001 <umawtips1@gmail.com>
2018-09-10 15:35:54 +0500

    dddf

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    googleClientID: '569225356441-873f9t6e2pjv643p5d3b6h7ff3co33fn.apps.googleusercontent.com',
-    googleClientSecret: '7tzWtjBlF1U9sySOUB1jIIFR',
+    googleClientID: '934984680334-vgq8mk64r4pfj9c2bfbbeigcs0m24qa1.apps.googleusercontent.com',
+    googleClientSecret: 'xE-BzYQNAEkPgiR30Ix4Nm1p',
     mongoURI: 'mongodb://olx-clone:getabc12345@ds249942.mlab.com:49942/olx-clone',
     cookieKey: 'dsfgjhdfjjdkgyg89745jkgh'
 }
